264136302ccd6ecc87422ff932a340f8f7af42b3
Oscar Bryant <oscarcbryant@gmail.com>
2022-05-04 10:17:43 +1000

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/Book-Search-Engine',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
