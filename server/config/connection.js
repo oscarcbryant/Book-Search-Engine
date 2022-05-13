20390ba778950139f65535c8311b1c1133769bd2
Oscar Bryant <oscarcbryant@gmail.com>
2022-05-14 00:57:48 +1000

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,6 +8,11 @@ mongoose.connect(
   },
 );
 
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
