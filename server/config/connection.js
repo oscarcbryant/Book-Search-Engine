668a3199644e3a7f4b3ba8bb004029344245f870
Oscar Bryant <oscarcbryant@gmail.com>
2022-05-15 20:57:19 +1000

    adding config

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,11 +8,11 @@ mongoose.connect(
   },
 );
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
 
 module.exports = mongoose.connection;
